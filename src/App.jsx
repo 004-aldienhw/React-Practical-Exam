825ed789e0c59e7cfa2b0b23188fcb9566d9b826
AldienHW <aldienhaidarwahyono@gmail.com>
2026-05-30 09:59:22 +0700

    Adding Userlist and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar"
 import UserCard from "./components/UserCard"
+import Userlist from "./components/Userlist"
+import Footer from "./components/Footer"
 
 function App() {
+  return(
   <>
   <Navbar></Navbar>
   <UserCard></UserCard>
+  <Userlist></Userlist>
+  <Footer></Footer>
   </>
+  )
 }
 
 export default App
---
+++ src/components/Userlist.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+import UserCard from "./UserCard";
+
+export default function Userlist () {
+    const { users } = useContext(UserContext);
+
+    return (
+        <>
+        <div>
+            {user.map((user) => (
+                <UserCard key={user.id} user={user} />
+            ))}
+        </div>
+        </>
+    )
+}
